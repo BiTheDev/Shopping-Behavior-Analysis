1433733bbc5a24c50fe933180b1c466dc0e9f266
Jaewoo <jaewoocho1017@gmail.com>
2023-12-11 11:41:35 -0800

    added some clustering

--- prediction.py
+++ prediction.py
@@ -4,6 +4,9 @@
 #   including the frequency of purchases, average review rating,and the types of items purchased?
 # %%
 
+from sklearn.metrics import silhouette_score
+import seaborn as sns
+from sklearn.impute import SimpleImputer
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import StandardScaler
 from sklearn.cluster import KMeans
@@ -17,6 +20,10 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error
 from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import accuracy_score, classification_report
 
 import pandas as pd
 from sklearn.preprocessing import LabelEncoder, StandardScaler
@@ -39,12 +46,28 @@ for column in categorical_columns:
     preprocessed_data[column] = label_encoders[column].fit_transform(
         preprocessed_data[column])
 
+preprocessed_data.fillna(0, inplace=True)
 # Normalizing numerical variables
 numerical_columns = [
     'Age', 'Review Rating', 'Previous Purchases']
 scaler = StandardScaler()
 preprocessed_data[numerical_columns] = scaler.fit_transform(
     preprocessed_data[numerical_columns])
+
+# Check for NaN values in each column
+nan_columns = preprocessed_data.columns[preprocessed_data.isna(
+).any()].tolist()
+
+# Check for infinite values in each column
+inf_columns = preprocessed_data.columns[(
+    preprocessed_data.abs() == np.inf).any()].tolist()
+
+# Combine both lists to get columns with either NaN or infinite values
+columns_with_issues = list(set(nan_columns + inf_columns))
+
+# Print columns with NaN or infinite values
+print("Columns with NaN or infinite values:", columns_with_issues)
+
 
 # %%
 # Need to visualize purchase amount distribution
@@ -62,20 +85,48 @@ preprocessed_data['Purchase Amount (USD)'].describe()
 # 1: 20-40, 2: 40-60, 3: 60-80, 4: 80-100
 # make a new column called 'Purchase Amount Group'
 preprocessed_data['Purchase Amount Group'] = pd.cut(
-    preprocessed_data['Purchase Amount (USD)'], bins=[20, 40, 60, 80, 100], labels=[1, 2, 3, 4])
+    preprocessed_data['Purchase Amount (USD)'], bins=[0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100], labels=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
 preprocessed_data['Purchase Amount Group'].value_counts()
 
 # I created a new column that categorize person into 4 groups based on their purchase amount
+preprocessed_data['Purchase Amount Group'].describe()
+preprocessed_data['Purchase Amount Group'].value_counts().plot(kind='bar')
 
+
+# %%
+selected_columns = ['Age', 'Gender', 'Location',  'Item Purchased',
+                    'Frequency of Purchases', 'Review Rating', 'Previous Purchases']
+
+X = preprocessed_data[selected_columns]
+y = preprocessed_data['Purchase Amount Group']  # Target variable
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.3, random_state=42)
+
+
+# Step 4: Train the KNN model
+k = 10  # Adjust the value of k as needed
+knn_model = KNeighborsClassifier(n_neighbors=k)
+knn_model.fit(X_train, y_train)
+
+# Step 5: Make predictions
+y_pred = knn_model.predict(X_test)
+
+# Step 6: Evaluate the model
+accuracy = accuracy_score(y_test, y_pred)
+print(f"Accuracy: {accuracy:.2f}")
+
+# Classification report for more detailed evaluation
+print(classification_report(y_test, y_pred))
 
 # %%
 
 # Assuming 'Target_Variable_Column_Name' is the column you want to predict
-target_variable = ''
+target_variable = 'Purchase Amount Group'
 
 # Selecting features for clustering (customize based on your dataset)
-selected_features = [
-    'Age', 'Subscription Status', 'Season', 'Frequency of Purchases', 'Review Rating', 'Previous Purchases']
+selected_features = ['Age', 'Gender', 'Location', 'Size', 'Season', 'Item Purchased',
+                     'Frequency of Purchases', 'Review Rating', 'Previous Purchases']
 
 # Extracting the selected features
 X = preprocessed_data[selected_features]
@@ -100,7 +151,7 @@ plt.ylabel('WCSS (Within-Cluster Sum of Squares)')
 plt.show()
 
 # Based on the Elbow method, choose the optimal number of clusters (let's say k=3)
-optimal_clusters = 3
+optimal_clusters = 10
 
 # Applying K-Means clustering
 kmeans = KMeans(n_clusters=optimal_clusters, init='k-means++',
@@ -111,84 +162,33 @@ preprocessed_data['Cluster'] = kmeans.fit_predict(X_scaled)
 for cluster in range(optimal_clusters):
     cluster_data = preprocessed_data[preprocessed_data['Cluster'] == cluster]
     plt.scatter(
-        cluster_data['Age'], cluster_data['Purchase Amount (USD)'], label=f'Cluster {cluster + 1}')
+        cluster_data['Age'], cluster_data['Purchase Amount Group'], label=f'Cluster {cluster + 1}')
 
 plt.scatter(kmeans.cluster_centers_[:, 0], kmeans.cluster_centers_[
             :, 1], s=300, c='red', marker='*', label='Centroids')
 plt.title('Customer Segmentation')
-plt.xlabel('Age')
-plt.ylabel('Purchase Amount (USD)')
+plt.xlabel('Gender')
+plt.ylabel('Purchase Amount Group')
 plt.legend()
 plt.show()
 
 
-# %%
-# Linear Regression
-# Import necessary libraries
+# Visualizing the clusters using a pair plot
+# cluster_palette = sns.color_palette("husl", optimal_clusters)
+# sns.pairplot(preprocessed_data, hue='Cluster', palette=cluster_palette)
+# plt.show()
 
-# Separate features (X) and target variable (y)
-X = preprocessed_data[['Age', 'Gender', 'Location', 'Size', 'Season', 'Item Purchased',
-                       'Color', 'Frequency of Purchases', 'Review Rating', 'Previous Purchases']]
-y = preprocessed_data['Purchase Amount (USD)']
+# # Evaluate clustering using silhouette score
+# silhouette_avg = silhouette_score(X_scaled, preprocessed_data['Cluster'])
+# print(f"Silhouette Score: {silhouette_avg}")
 
-# Split the data into training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.2, random_state=42)
-
-# Initialize the Linear Regression model
-model = LinearRegression()
-
-# Train the model
-model.fit(X_train, y_train)
-
-# Make predictions on the test set
-y_pred = model.predict(X_test)
-coefficients = pd.DataFrame({
-    'Feature': X.columns,
-    'Coefficient': model.coef_
-})
-
-# Display coefficients sorted by magnitude
-coefficients = coefficients.reindex(
-    coefficients['Coefficient'].abs().sort_values(ascending=False).index)
-print(coefficients)
-# Evaluate the model
-mse = mean_squared_error(y_test, y_pred)
-r2 = r2_score(y_test, y_pred)
-print(f'Mean Squared Error: {mse}')
-print(f'R-squared: {r2}')
-
-# # Example: Predicting the purchase amount for a new customer
-# new_customer_data = pd.DataFrame({
-#     'Age': [30],
-#     'Gender': ['Male'],
-#     'Location': ['New York'],
-#     'Size': ['M'],
-#     'Season': ['Fall'],
-#     'Item Purchased': ['Sweater'],
-#     'Color': ['Blue'],
-#     'Frequency of Purchases': [10],
-#     'Review Rating': [4.0],
-#     'Previous Purchases': [20]
-# })
-
-# # Encode categorical variables for the new customer
-# for column in categorical_columns:
-#     new_customer_data[column] = label_encoders[column].transform(
-#         new_customer_data[column])
-
-# # Make a prediction for the new customer
-# new_customer_prediction = model.predict(new_customer_data)
-
-# print(
-#     f'Predicted Purchase Amount for the New Customer: {new_customer_prediction[0]}')
 
 # %%
 # second try
 
 
-X = preprocessed_data[['Age', 'Gender', 'Location',
-                       'Season']]
+X = preprocessed_data['Age', 'Gender', 'Location',
+                      'Season']
 y = preprocessed_data['Purchase Amount (USD)']
 
 # Split the data into training and testing sets
@@ -225,8 +225,9 @@ print(f'R-squared: {r2}')
 # Import necessary libraries
 
 # Separate features (X) and target variable (y)
-X = preprocessed_data[['Age', 'Gender', 'Location']]
-y = preprocessed_data['Purchase Amount (USD)']
+X = preprocessed_data['Age', 'Subscription Status', 'Season',
+                      'Frequency of Purchases', 'Review Rating', 'Previous Purchases']
+y = preprocessed_data['Purchase Amount Group']
 
 # Split the data into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(
@@ -242,13 +243,13 @@ model.fit(X_train, y_train)
 y_pred = model.predict(X_test)
 
 # Evaluate the model
-mse = mean_squared_error(y_test, y_pred)
-r2 = r2_score(y_test, y_pred)
+# mse = mean_squared_error(y_test, y_pred)
+# r2 = r2_score(y_test, y_pred)
 
-# accuracy = accuracy_score(y_test, y_pred)
-# print(f'accuracy: {accuracy}')
-print(f'Mean Squared Error: {mse}')
-print(f'R-squared: {r2}')
+accuracy = accuracy_score(y_test, y_pred)
+print(f'accuracy: {accuracy}')
+# print(f'Mean Squared Error: {mse}')
+# print(f'R-squared: {r2}')
 
 # # Example: Predicting the purchase amount for a new customer
 # new_customer_data = pd.DataFrame({
