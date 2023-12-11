4ff3062e4cc1ccf1c665917ea9db9d2c2e2fdd39
Jaewoo <jaewoocho1017@gmail.com>
2023-12-11 10:46:11 -0800

    purchase amount group added and customer cluster

--- prediction.py
+++ prediction.py
@@ -4,6 +4,9 @@
 #   including the frequency of purchases, average review rating,and the types of items purchased?
 # %%
 
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import StandardScaler
+from sklearn.cluster import KMeans
 import numpy as np
 from sklearn.metrics import mean_squared_error, r2_score
 from sklearn.linear_model import LinearRegression
@@ -38,10 +41,85 @@ for column in categorical_columns:
 
 # Normalizing numerical variables
 numerical_columns = [
-    'Age', 'Purchase Amount (USD)', 'Review Rating', 'Previous Purchases']
+    'Age', 'Review Rating', 'Previous Purchases']
 scaler = StandardScaler()
 preprocessed_data[numerical_columns] = scaler.fit_transform(
     preprocessed_data[numerical_columns])
+
+# %%
+# Need to visualize purchase amount distribution
+# Visualizing the distribution of the target variable
+plt.hist(preprocessed_data['Purchase Amount (USD)'])
+plt.title('Purchase Amount Distribution')
+plt.xlabel('Purchase Amount (USD)')
+plt.ylabel('Count')
+plt.show()
+# stats for purchase amount
+preprocessed_data['Purchase Amount (USD)'].describe()
+
+# we can group from 20-40, 40-60, 60-80, 80-100
+# I want to categorize the purchase amount into 4 groups
+# 1: 20-40, 2: 40-60, 3: 60-80, 4: 80-100
+# make a new column called 'Purchase Amount Group'
+preprocessed_data['Purchase Amount Group'] = pd.cut(
+    preprocessed_data['Purchase Amount (USD)'], bins=[20, 40, 60, 80, 100], labels=[1, 2, 3, 4])
+preprocessed_data['Purchase Amount Group'].value_counts()
+
+# I created a new column that categorize person into 4 groups based on their purchase amount
+
+
+# %%
+
+# Assuming 'Target_Variable_Column_Name' is the column you want to predict
+target_variable = ''
+
+# Selecting features for clustering (customize based on your dataset)
+selected_features = [
+    'Age', 'Subscription Status', 'Season', 'Frequency of Purchases', 'Review Rating', 'Previous Purchases']
+
+# Extracting the selected features
+X = preprocessed_data[selected_features]
+
+# Standardizing the data
+scaler = StandardScaler()
+X_scaled = scaler.fit_transform(X)
+
+# Determining the optimal number of clusters using the Elbow method
+wcss = []
+for i in range(1, 11):
+    kmeans = KMeans(n_clusters=i, init='k-means++',
+                    max_iter=300, n_init=10, random_state=0)
+    kmeans.fit(X_scaled)
+    wcss.append(kmeans.inertia_)
+
+# Plotting the Elbow method
+plt.plot(range(1, 11), wcss)
+plt.title('Elbow Method')
+plt.xlabel('Number of Clusters')
+plt.ylabel('WCSS (Within-Cluster Sum of Squares)')
+plt.show()
+
+# Based on the Elbow method, choose the optimal number of clusters (let's say k=3)
+optimal_clusters = 3
+
+# Applying K-Means clustering
+kmeans = KMeans(n_clusters=optimal_clusters, init='k-means++',
+                max_iter=300, n_init=10, random_state=0)
+preprocessed_data['Cluster'] = kmeans.fit_predict(X_scaled)
+
+# Visualizing the clusters (customize based on your dataset)
+for cluster in range(optimal_clusters):
+    cluster_data = preprocessed_data[preprocessed_data['Cluster'] == cluster]
+    plt.scatter(
+        cluster_data['Age'], cluster_data['Purchase Amount (USD)'], label=f'Cluster {cluster + 1}')
+
+plt.scatter(kmeans.cluster_centers_[:, 0], kmeans.cluster_centers_[
+            :, 1], s=300, c='red', marker='*', label='Centroids')
+plt.title('Customer Segmentation')
+plt.xlabel('Age')
+plt.ylabel('Purchase Amount (USD)')
+plt.legend()
+plt.show()
 
 
 # %%
@@ -137,6 +215,8 @@ print(coefficients)
 # Evaluate the model
 mse = mean_squared_error(y_test, y_pred)
 r2 = r2_score(y_test, y_pred)
+# accuracy = accuracy_score(y_test, y_pred)
+# print(f'accuracy: {accuracy}')
 print(f'Mean Squared Error: {mse}')
 print(f'R-squared: {r2}')
 
@@ -165,6 +245,8 @@ y_pred = model.predict(X_test)
 mse = mean_squared_error(y_test, y_pred)
 r2 = r2_score(y_test, y_pred)
 
+# accuracy = accuracy_score(y_test, y_pred)
+# print(f'accuracy: {accuracy}')
 print(f'Mean Squared Error: {mse}')
 print(f'R-squared: {r2}')
 
